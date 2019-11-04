5014bf7bc28cd1f1ef243e64cd5d4fd2bc325da4
Jessen <jessendasilva1@gmail.com>
2019-11-04 18:47:47 -0500

    configured nodemailer

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,40 @@
+const nodeMailer = require('nodemailer');
+
 module.exports = function(app) {
-    app.post("/contactInfo", function(req, res) {
-        let data = req.body.data;
-        console.log(data.name);
-        console.log("Name: " + data.name + ". Email: " + data.email + ". Message: " + data.message + ".");
-        console.log("backend reached!");
-        //console.log(req.query.hello);
-        res.send("success response from server");
-    });
-  
-    //The 404 Route (ALWAYS Keep this as the last route)
-    app.get("*", function(req, res) {
-      res.send("404");
-    });
-  };
+	app.post("/contactInfo", function(req, res) {
+		let data = req.body.data;
+		console.log(data.name);
+		console.log("Name: " + data.name + ". Email: " + data.email + ". Message: " + data.message + ".");
+		console.log("backend reached!");
+
+		let transporter = nodeMailer.createTransport({
+			host: 'smtp.gmail.com',
+			port: 465,
+			secure: true,
+			auth: {
+				user: 'jessendasilva1@gmail.com',
+				pass: "ahjqzhbboafhrjsc"
+			}
+		});
+		let mailOptions = {
+			replyTo: data.name + " " + data.email,
+			to: 'jessendasilva1@gmail.com',
+			subject: "Inquiry from Portfolio",
+			text: data.message
+		};
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				return console.log(error);
+			}
+			console.log('Message %s sent: %s', info.messageId, info.response);
+		});
+		//console.log(req.query.hello);
+		res.send("success response from server");
+	});
+
+	//The 404 Route (ALWAYS Keep this as the last route)
+	app.get("*", function(req, res) {
+		res.send("404");
+	});
+};
   
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const nodeMailer = require('nodemailer');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
