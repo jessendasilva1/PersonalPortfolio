8abd838f2cb0191d3e38dee12b852c6c941c9915
jessen <jessen@uoguelph.ca>
2022-11-08 11:13:28 -0500

    added image to University Search component, fixed build error on Heroku

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
     render(){
         return(
             <div className="footer">
-               <span id="footerName">Jessen Dasilva <span id="footerDate">@2019</span></span>
+               <span id="footerName">Jessen Dasilva <span id="footerDate">@2022</span></span>
             </div>
         );
     }
